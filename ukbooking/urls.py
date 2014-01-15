884abe783aca87a409ba560366b22f5b3ee6fc33
pgjones <p.g.jones@qmul.ac.uk>
2014-01-15 13:50:55 +0000

    Bug fix

--- ukbooking/urls.py
+++ ukbooking/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from booking import views
+from ukbooking import views
 
 urlpatterns = patterns('',
                        #url(r'^$', views.index, name='index'),
