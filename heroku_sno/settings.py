4e391a12a6c738c713ca1e637d917b78f6e99a84
pgjones <p.g.jones@qmul.ac.uk>
2014-01-15 13:58:37 +0000

    Fix template dir bug.

--- heroku_sno/settings.py
+++ heroku_sno/settings.py
@@ -108,6 +108,7 @@ ROOT_URLCONF = 'heroku_sno.urls'
 WSGI_APPLICATION = 'heroku_sno.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), "templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
