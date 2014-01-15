7ca40837091698bb74f320a912c36d5097401f16
pgjones <p.g.jones@qmul.ac.uk>
2014-01-15 13:48:13 +0000

    First stage, apartments.

--- ukbooking/urls.py
+++ ukbooking/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url
 
-#from booking import views
+from booking import views
 
-urlpatterns = patterns('')
+urlpatterns = patterns('',
+                       #url(r'^$', views.index, name='index'),
+
+                       url(r'^apartments/$', views.Apartments.as_view(), name='apartments'),
+                       url(r'^apartment/(?P<apartment_id>\d+)/$', views.apartment, name='apartment'),
+)
--- ukbooking/views.py
+++ ukbooking/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import logout
+from django.views import generic
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+
+import calendar
+import datetime
+import time
+
+from ukbooking.models import Apartment, Bed, Booking, Visit
+#from snolab_booking.forms import VisitForm
+
+class Apartments(generic.ListView):
+    model = Apartment
+    template_name = "ukbooking/apartments.html"
+
+def apartment(request, apartment_id):
+    """ Return apartment information."""
+    apartment = get_object_or_404(Apartment, pk=apartment_id)
+    try:
+        beds = Bed.objects.filter(apartment=apartment_id)
+        return render(request, 'ukbooking/apartment.html', {'apartment' : apartment, 'bed_list' : beds})
+    except Bed.DoesNotExist:
+        return render(request, 'ukbooking/apartment.html', {'apartment' : apartment, 'bed_list' : []})
