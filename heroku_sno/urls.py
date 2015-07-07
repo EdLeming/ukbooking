9bdc5923f86f1ddb8e554b0b599bfff52fecd15f
EdLeming <e.leming09@googlemail.com>
2015-07-07 01:28:01 +0100

    added redirect to heroku_sno/url.py

--- heroku_sno/urls.py
+++ heroku_sno/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       #url(r'^ukbooking/', include('ukbooking.urls')),
-                       url(r'^$', include('ukbooking.urls')),
+                       url(r'^ukbooking/', include('ukbooking.urls')),
+                       url(r'^$', redirect_to, {'url':'/ukbooking/'}),
                        url(r'^admin/', include(admin.site.urls)),
 )
